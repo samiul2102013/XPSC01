ce012730ac5f310776926f29c1e3da2056556bcf
Samiul Hasan <111248587+samiul2102013@users.noreply.github.com>
2023-07-21 23:37:14 +0600

    after added

--- Week 7/day 1/A_Mocha_and_Math.cpp
+++ Week 7/day 1/A_Mocha_and_Math.cpp
@@ -9,16 +9,11 @@ void solve(){
         cin>>k;
         v.push_back(k);
       }
-      vector<int>ans;
-      for(int i=0,j=v.size()-1;i<v.size();i++,j--){
-        int p = (v[i]&v[j]);
-        ans.push_back(p);
+      int result = v[0];
+      for(int i=1;i<n;i++){
+        result&=v[i];
       }
-      int mini = INT_MAX;
-      for(int i=0;i<ans.size();i++){
-        mini = min(mini,ans[i]);
-      }
-      cout<<mini<<endl;
+      cout<<result<<endl;
     
 }
 int main(){
